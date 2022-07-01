df963920c9ad9e498f39dd27a9387763d98273a8
yehudz <yehuda.zeytim@gmail.com>
2022-06-30 19:40:37 -0600

    Add Primary button to UI components

---
+++ src/lib/ui/PrimaryButton.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let buttonText: string = 'Learn More';
+  export let buttonColor: string = 'white'
+</script>
+
+<button class={`primaryButton ${buttonColor}`}>{buttonText}</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
+<script>
+import PrimaryButton from "$lib/ui/PrimaryButton.svelte";
+
+</script>
 <h1>The quick brown fox jumps over the lazy dog</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<PrimaryButton buttonText={'Get In Touch'} buttonColor={"peach"}/>
