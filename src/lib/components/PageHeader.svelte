5aab905537b5139ad3435ef2ee38d7168de989ee
yehudz <yehuda.zeytim@gmail.com>
2022-07-05 10:13:17 -0600

    Add page header component

---
+++ src/lib/components/PageHeader.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let title: string
+  export let text: string
+</script>
+
+<div class="pageHeader">
+  <div class="title">{title}</div>
+  <p class="body-2">{text}</p>
+</div>
---
+++ src/routes/web-design.svelte
@@ -0,0 +1,5 @@
+<script>
+import PageHeader from "$lib/components/PageHeader.svelte";
+
+</script>
+<PageHeader title={"Web Design"} text={"We build websites that serve as powerful marketing tools and bring memorable brand experiences."}/>
