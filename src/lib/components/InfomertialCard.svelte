c82a284e72e634c40237171d3a4b3cb133f421e1
yehudz <yehuda.zeytim@gmail.com>
2022-07-04 19:45:51 -0600

    Add infomertial section

---
+++ src/lib/components/InfomertialCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let image: string
+  export let title: string
+  export let text: string
+</script>
+
+<div class="infomertialContainer">
+  <div class="infomertialImageContainer">
+    <img class="bg" src="assets/home/desktop/bg-pattern-hero-home.svg" alt="">
+    <img class="img" src={image} alt={title}>
+  </div>
+  <div class="textContent">
+    <h3>{title}</h3>
+    <p class="body-2">{text}</p>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script>
   import HomeHeroSection from "$lib/layout/HomeHeroSection.svelte";
+import InfomertialSection from "$lib/layout/InfomertialSection.svelte";
   import ViewProjectsSection from "$lib/layout/ViewProjectsSection.svelte";
 </script>
 
 <HomeHeroSection />
 <ViewProjectsSection />
+<InfomertialSection />
