f28fe0c6b36d2ef02232b339c948fa4c28fb1a65
yehudz <yehuda.zeytim@gmail.com>
2022-07-05 13:15:12 -0600

    Add the about page

---
+++ src/routes/about-us.svelte
@@ -0,0 +1,17 @@
+<div class="aboutUsHeroSection">
+  <div class="image"></div>
+  <div class="content">
+    <div class="title">About Us</div>
+    <div class="text">Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences.</div>
+  </div>
+</div>
+<div class="aboutUsSubSection">
+  <div class="image"></div>
+  <div class="content">
+    <div class="title">World-class talent</div>
+    <div class="text">We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.   
+      <br>
+      <br>
+      Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.</div>
+  </div>
+</div>
