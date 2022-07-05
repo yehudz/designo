e2b22c28ee80db41262659217fb521d9d82ee17e
yehudz <yehuda.zeytim@gmail.com>
2022-07-05 17:40:16 -0600

    Add last section for about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -19,3 +19,14 @@
   </div>
 </div>
 <Locations />
+<div class="aboutUsLowerSection">
+  <div class="image"></div>
+  <div class="content">
+    <div class="title">The real deal</div>
+    <div class="text">As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own.
+      Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.
+      <br>
+      <br>
+      We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.</div>
+  </div>
+</div>
