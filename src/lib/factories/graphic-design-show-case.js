503e84a2100ee7c47d17cf4339b457108f58fc5d
yehudz <yehuda.zeytim@gmail.com>
2022-07-05 11:08:34 -0600

    Add the graphic design page

---
+++ src/lib/factories/graphic-design-show-case.js
@@ -0,0 +1,19 @@
+const graphicdesignShowcaseFactory = [
+  {
+    image: "assets/graphic-design/desktop/image-change.jpg",
+    title: "Tim Brown",
+    subtitle: "A book cover designed for Tim Brown’s new release, ‘Change’"
+  },
+  {
+    image: "assets/graphic-design/desktop/image-boxed-water.jpg",
+    title: "Boxed Water",
+    subtitle: "A simple packaging concept made for Boxed Water"
+  },
+  {
+    image: "assets/graphic-design/desktop/image-science.jpg",
+    title: "SCIENCE!",
+    subtitle: "A poster made in collaboration with the Federal Art Project"
+  }
+]
+
+export default graphicdesignShowcaseFactory
---
+++ src/routes/graphic-design.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+  import PageHeader from "$lib/components/PageHeader.svelte";
+  import ShowCaseCard from "$lib/components/ShowCaseCard.svelte";
+  import ViewProjectCard from "$lib/components/ViewProjectCard.svelte";
+  import graphicdesignShowCaseData from "$lib/factories/graphic-design-show-case";
+</script>
+<PageHeader title={"Graphic Design"} text={"We deliver eye-catching branding materials that are tailored to meet your business objectives."}/>
+<div class="showCase">
+  {#each graphicdesignShowCaseData as item}
+    <ShowCaseCard 
+      image={item.image}
+      title={item.title}
+      subtitle={item.subtitle}
+    />
+  {/each}
+  <div class="viewProjectsContainer">
+    <ViewProjectCard 
+      title={"App Design"} 
+      image={'/assets/home/mobile/image-app-design.jpg'}
+      url={"app-design"}
+    />
+    <ViewProjectCard 
+      title={"Web Design"} 
+      image={'/assets/home/mobile/image-web-design.jpg'}
+      url={"web-design"}
+    />
+  </div>
+</div>
