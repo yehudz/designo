bf92f45ee8efacad33fd36355a8c242a5d011f24
yehudz <yehuda.zeytim@gmail.com>
2022-07-05 10:51:07 -0600

    Add other projects cards

--- src/routes/web-design.svelte
+++ src/routes/web-design.svelte
@@ -1,6 +1,7 @@
 <script>
   import PageHeader from "$lib/components/PageHeader.svelte";
   import ShowCaseCard from "$lib/components/ShowCaseCard.svelte";
+import ViewProjectCard from "$lib/components/ViewProjectCard.svelte";
   import webdesignShowCaseData from "$lib/factories/web-design-show-case";
 </script>
 <PageHeader title={"Web Design"} text={"We build websites that serve as powerful marketing tools and bring memorable brand experiences."}/>
@@ -12,4 +13,16 @@
       subtitle={item.subtitle}
     />
   {/each}
+  <div class="viewProjectsContainer">
+    <ViewProjectCard 
+      title={"App Design"} 
+      image={'/assets/home/mobile/image-app-design.jpg'}
+      url={"app-design"}
+    />
+    <ViewProjectCard 
+      title={"Graphic Design"} 
+      image={'/assets/home/mobile/image-graphic-design.jpg'}
+      url={"graphic-design"}
+    />
+  </div>
 </div>
