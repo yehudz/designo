a71b47402ad8bfcda9f1a11b9d0fdd089af77567
yehudz <yehuda.zeytim@gmail.com>
2022-07-05 21:37:40 -0600

    Add toggle behavior to mobile menu

--- src/lib/layout/TopBar.svelte
+++ src/lib/layout/TopBar.svelte
@@ -2,10 +2,14 @@
   import MobileMenu from "$lib/ui/MobileMenu.svelte"
 	import { mobileMenuVisibility } from '$lib/context/mobileMenuVisibility';
   export let isMobile: boolean = true
+  let imageSrc = "/assets/shared/mobile/icon-hamburger.svg"
   function toggleMenu() {
     mobileMenuVisibility.update(n=> n = !n)
+    if ($mobileMenuVisibility) imageSrc = "/assets/shared/mobile/icon-close.svg"
+    else imageSrc = "/assets/shared/mobile/icon-hamburger.svg"
     document.body.classList.toggle('noScroll'); 
   }
+  console.log(imageSrc)
 </script>
 <div class="topBarContainer">
   <a href="/" class="logoContainer">
@@ -13,10 +17,10 @@
   </a>
   {#if isMobile}
     <div class="hamburgerMenu" on:click={toggleMenu}>
-      <img src="/assets/shared/mobile/icon-hamburger.svg" alt="Menu Icon">
+      <img src={imageSrc} alt="Menu Icon">
     </div>
   {/if}
 </div>
 {#if $mobileMenuVisibility}
-  <MobileMenu />
+  <MobileMenu {toggleMenu}/>
 {/if}
--- src/lib/ui/MobileMenu.svelte
+++ src/lib/ui/MobileMenu.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-  import { fade, fly } from 'svelte/transition';
+  import { fly } from 'svelte/transition';
   import { mobileMenuVisibility } from '$lib/context/mobileMenuVisibility';
-  function navigate() {
-    document.body.classList.toggle('noScroll');
-    mobileMenuVisibility.update(n=> n = !n)
-  }
+  export let toggleMenu: ()=> void
 </script>
 
 <div class="mobileMenuContainer">
   <div class="menuList">
-    <a in:fly={{y: -50, duration: 550}} on:click={navigate} href="/about">Our Company</a>
-    <a in:fly={{y: -50, duration: 550, delay: 100}} on:click={navigate} href="/locations">Locations</a>
-    <a in:fly={{y: -50, duration: 550, delay: 200}} on:click={navigate} href="/contact">Contact</a>
+    <a in:fly={{y: -50, duration: 550}} on:click={toggleMenu} href="/about">Our Company</a>
+    <a in:fly={{y: -50, duration: 550, delay: 100}} on:click={toggleMenu} href="/locations">Locations</a>
+    <a in:fly={{y: -50, duration: 550, delay: 200}} on:click={toggleMenu} href="/contact">Contact</a>
   </div>
   <div class="overlay"></div>
 </div>
